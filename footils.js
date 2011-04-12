b5ff60cb5a362e2b5778b687d41ab51ec9f54063
Mike <mike@mikepennisi.com>
2011-04-12 10:33:44 -0400

    Implemented forEach and started test cases

--- footils.js
+++ footils.js
@@ -2,21 +2,31 @@
 
 	//	Create localized reference to global context
 	var global = this,
-
+		forEach = Array.prototype.forEach;
+		
 	// Set up our "util" module
-			Footils = (function () {
-
-				//	normally, we might do something important in here
-				return {
-
-					forEach: function( obj, fn, context ) {
-
+		Footils = (function () {
+		
+			//	normally, we might do something important in here
+			return {
+			
+				forEach: function( obj, fn, context ) {
+					context = context || this;
+					if ( forEach && obj.forEach ) {
+						return obj.forEach( fn, context );
 					}
+					for ( var prop in obj ) {
+						if ( obj.hasOwnProperty(prop) ) {
+							fn.call( context, obj[prop], prop );
+						}
+					}
+					return obj;
+				}
 
-				};
+			};
 
 
-			})();
+		})();
 
 	global.Footils = global.F = Footils;
 })();
--- qunit/test/footils.unit.js
+++ qunit/test/footils.unit.js
@@ -3,8 +3,32 @@
 	ok(true);
 });*/
 
-test("expect in test", 3, function() {
-	ok(true);
-	ok(true);
-	ok(true);
+test("foreach implementation", 2, function() {
+	// Touch every member of the included array
+	var foo = [1,2,3],
+		counter = 0;
+	
+	F.forEach(foo, function(value, index) {
+		counter++;
+	});
+	equal(counter, foo.length, "F.forEach touches every member of the array");
+	
+	// Touch every member of the included object
+	var foo = {
+			"foobar": true,
+			bar: true,
+			"gasfka": true
+		},
+		counter = 0;
+	
+	F.forEach(foo, function(value, index) {
+		counter++;
+	});
+	equal(counter, 3, "F.forEach touches every member of the object");
 });
+
+// Accepts and array or object
+	// Accepts an apply function
+	// Accepts the context
+	
+	// Return: the object iterated over
